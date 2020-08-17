0ddbcfc9dc276870b8decc30eb17f3701ebd635d
Javier <ingenierodesoftware73@gmail.com>
2020-08-17 21:09:38 +0000

    delete line

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,14 +6,14 @@ web static
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """
-    Function that generate a .tgz 
+    Function that generate a .tgz
     """
     time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
     file_name = "version/web_static{}.tgz".format(time)
     try:
-        local("mkdir -p ./version")
         local("tar -zcvf {} ./web_static".format(file_name))
         return file_name
     except:
