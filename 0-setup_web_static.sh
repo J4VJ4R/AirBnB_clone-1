fb6e88087c7d0b58ca722a5fb7fa4c3f7c7fba4b
Javier <ingenierodesoftware73@gmail.com>
2020-08-17 18:49:52 +0000

    Preparating web server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#Preparating web server
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/shared/test
+sudo bash -c /data/web_static/releases/test
+sudo bash -c 'echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html>" > /data/web_static/releases/test/index.html'
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i 'sudo sed -i '/^[\t ]*location \// i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default'
+sudo service nginx restart
