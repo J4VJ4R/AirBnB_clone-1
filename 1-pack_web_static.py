a3cec3088c0c4b6ab54ae4c16b81b37d9224eebc
Javier <ingenierodesoftware73@gmail.com>
2020-08-17 21:45:40 +0000

    agree sudo

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,7 +3,7 @@
 Fabric script for create a zip file for all
 web static
 """
-from fabric.api import local
+from fabric.api import local, run, sudo
 from datetime import datetime
 
 
@@ -12,9 +12,10 @@ def do_pack():
     Function that generate a .tgz
     """
     time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    file_name = "version/web_static{}.tgz".format(time)
+    file_name = "version/web_static_{}.tgz".format(time)
     try:
-        local("tar -zcvf {} ./web_static".format(file_name))
+        local("sudo mkdir -p version")
+        local("sudo tar -zcvf {} ./web_static".format(file_name))
         return file_name
     except:
         return None
