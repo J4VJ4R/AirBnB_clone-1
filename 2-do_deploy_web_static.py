b9aebeb31e747a7bb100aa8ca73b81879cd4a311
Javier <ingenierodesoftware73@gmail.com>
2020-08-18 01:25:14 +0000

    missing indentation

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,22 +14,23 @@ def do_deploy(archive_path):
     """
     if os.path.isfile(archive_path) is False:
         return False
-    archive = archive_path.split("/")[-1]
-    path = "/data/web_static/releases"
-    put("{}".format(archive_path), "/tmp/{}".format(archive))
-    folder = arhive.split(".")
-    run("mkdir -p {}/{}/".format(path, folder[0]))
-    new_archive = '.'.join(folder)
-    run("tar -xzf /tmp{} -C {}/{}/"
-        .format(new_archive, path, folder[0]))
-    run("rm /tmp/{}".format(archive))
-    run("mv {}/{}/web_static/* {}/{}/"
-        .format(path, folder[0], path, folder[0]))
-    run("rm -rf {}/{}/web_static".format(path, folder[0]))
-    run("rm -rf /data/web_static/current")
-    run("ln -sf {}/{} /data/web_static/current"
-        .format(path, folder[0]))
-    print("New versión deployed!")
-    return True
-except:
-    return False
+    try:
+        archive = archive_path.split("/")[-1]
+        path = "/data/web_static/releases"
+        put("{}".format(archive_path), "/tmp/{}".format(archive))
+        folder = arhive.split(".")
+        run("mkdir -p {}/{}/".format(path, folder[0]))
+        new_archive = '.'.join(folder)
+        run("tar -xzf /tmp{} -C {}/{}/"
+            .format(new_archive, path, folder[0]))
+        run("rm /tmp/{}".format(archive))
+        run("mv {}/{}/web_static/* {}/{}/"
+            .format(path, folder[0], path, folder[0]))
+        run("rm -rf {}/{}/web_static".format(path, folder[0]))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf {}/{} /data/web_static/current"
+            .format(path, folder[0]))
+        print("New versión deployed!")
+        return True
+    except:
+        return False
