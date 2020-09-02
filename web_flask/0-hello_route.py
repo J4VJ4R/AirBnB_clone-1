d8cf449599663403bbef2fd329560e0da79bec12
Javier <ingenierodesoftware73@gmail.com>
2020-09-02 03:52:29 +0000

    pep8 ready

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,12 +5,14 @@ This code is to create a test with Flask
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_world():
     """
     this functión use slash '/' to show result or return some
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run()
